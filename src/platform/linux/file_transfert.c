a77d6c15aa4f5e04f6345a543e09ecd4e5b1586d
YaogoGerard <gerardwyaogo@gmail.com>
2026-04-28 13:13:13 +0000

    renomage du fichier socket.c en discovery.c et ajout d'un fichier file_transfert.c pour la logique de transfere des fichiers

---
+++ src/platform/linux/file_transfert.c
@@ -0,0 +1,8 @@
+ 
+
+
+
+/*
+Dans cette partie , je vais creé des helpers pour evité de repeter la logique de lecture 
+ou d'ecriture des fichiers ou meme  eviter d'avoir des fichiers corrompus
+*/
--- src/platform/linux/network.c
+++ src/platform/linux/network.c
@@ -62,6 +62,17 @@ int accept_client(int socket_tcp){
             return -1;
         }
         return client_socket;
+    // la fonction va retouner -1 , si le  server accept la connexion
+}
+
+// là on permet au serveur de refuser une connection entrante,en acceptant et en coupant la connexion imediatement
+int denied_client(int socket_tcp){
+    int client_socket=accept(socket_tcp,NULL,NULL);// je ne renvoie  ici que le socket du client, c'est pour cette raison que l'ip et le port sont à NULL
+    if (client_socket>=0) {
+        close(client_socket);
+        }
+        return client_socket;
+    // la focntion return une valeur >=0 car la connection sera fermé à la fin
 }
 
 // ici c'est dans le cas on veut se connecter à un autre server
